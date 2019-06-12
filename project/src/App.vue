193496760deb496e565cc8687e4a0b19eb0b58c5
JJH19 <jiangjiaheng1994@qq.com>
2019-06-12 21:13:27 +0800

    update project

---
+++ project/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <my-nav></my-nav>
+      </el-header>
+      <el-main>
+        <router-view class="view"/>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import MyNav from './layout/MyNav'
+
+export default {
+  name: 'app',
+  components: {
+    MyNav
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0 !important;
+  padding: 0 !important;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.el-header{
+  padding: 0 !important;
+}
+</style>
---
+++ project/src/layout/MyNav.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class>
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+    >
+      <el-menu-item index="1">
+        <router-link to="/">首页</router-link>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <router-link to="/about">详情</router-link>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <a href="" target="_blank">关于</a>
+      </el-menu-item>
+      <el-submenu index="4">
+        <template slot="title">更多</template>
+        <el-menu-item index="4-1">设置</el-menu-item>
+        <el-menu-item index="4-2">其他</el-menu-item>
+      </el-submenu>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {
+      activeIndex: "1"
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
